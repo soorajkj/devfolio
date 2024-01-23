765a8a40b2e87c02180a88afe21e5ec5dab89e93
soorajkj <soorajkj46@gmail.com>
2024-01-23 13:11:47 +0530

    chore(husky): commit check

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,18 +1,7 @@
-import Project from '@components/project';
-
 export default function Projects() {
   return (
     <section className="df-projects">
-      <div className="container">
-        <h2></h2>
-        <div className="df-projects__grid">
-          {Array.from({ length: 9 })
-            .fill(0)
-            .map((_item, idx) => {
-              return <Project key={idx} />;
-            })}
-        </div>
-      </div>
+      <div className="container">Projects</div>
     </section>
   );
 }
