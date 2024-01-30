0578b3e8545832af9320162aa48ca20e6b58d1cf
soorajkj <soorajkj46@gmail.com>
2024-01-31 00:32:33 +0530

    chore: fonts import/append fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import type { Metadata } from "next";
 import { LayoutProps } from "@types";
-import { bebasNeue, montserrat, robotoMono } from "@lib/fonts";
+import * as fonts from "@lib/fonts";
+import { cx } from "@lib/utils";
 import "@styles/app.css";
 
 export const metadata: Metadata = {};
@@ -13,11 +14,12 @@ export default function Layout(props: LayoutProps) {
     <html
       lang="en"
       suppressHydrationWarning={true}
-      style={{ scrollBehavior: "smooth" }}
-      className={`
-      ${montserrat.variable}
-      ${robotoMono.variable}
-      ${bebasNeue.variable}`}
+      className={cx(`
+      ${fonts.montserrat.variable}
+      ${fonts.robotoMono.variable}
+      ${fonts.bebasNeue.variable}
+      scroll-smooth
+      `)}
     >
       <body className="h-full min-h-screen w-full overflow-x-hidden font-family-inter text-base font-normal leading-normal antialiased">
         {children}
