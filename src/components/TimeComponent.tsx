82ed54fb3088a16e285cb5cf31183675f90ddf97
soorajkj <soorajkj46@gmail.com>
2025-08-09 15:29:24 +0530

    move to client comp

--- src/components/TimeComponent.tsx
+++ src/components/TimeComponent.tsx
@@ -3,12 +3,18 @@
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
+import { useIsMounted } from "usehooks-ts";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
 export default function TimeComponent() {
+  const isMounted = useIsMounted();
+
   const tz = "Etc/GMT+12";
   const time = dayjs().tz(tz).format("HH:mm [(UTC] Z[)]");
+
+  if (!isMounted) return null;
+
   return <span>{time}</span>;
 }
